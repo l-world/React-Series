bd59e63b75a886aa74a28dbe7e7546a8aba3f371
echo <heart0magic@163.com>
2023-03-22 09:37:30 +0800

    添加注释

--- src/components/ChessCmp/ChessCmp.tsx
+++ src/components/ChessCmp/ChessCmp.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { ChessType } from '../../types/enums';
 import './ChessCmp.css';
 
+// 使用接口定义 props 的类型约束,props有属性，1.type:棋子的类型，由ChessType约束，2. onClick：函数
 interface Iprops {
     type: ChessType,
     onClick: () => void
 }
 
+// 棋子组件
 const ChessCmp = ({ type, onClick }: Iprops) => {
     let chess = null;
+    // 根据棋子类型返回棋子
     if(type === ChessType.red){
         chess = <div className='red chess-item'></div>
     }else if(type === ChessType.black){
--- src/types/enums.ts
+++ src/types/enums.ts
@@ -1,3 +1,4 @@
+// 枚举定义棋子类型，三种：红色棋子，黑色棋子，空棋子
 export enum ChessType {
     none,
     red,
