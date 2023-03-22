cb6a4d8a510b1e68c2ce8bf5eedfb21e74085395
echo <heart0magic@163.com>
2023-03-22 17:28:19 +0800

    修复Game组件bug

--- src/components/GameCmp/index.tsx
+++ src/components/GameCmp/index.tsx
@@ -6,15 +6,15 @@ import './index.css'
 // 定义 state 的约束
 interface IState {
     chesses: ChessType[]
-    gameStatus:GameStatus
+    gameStatus: GameStatus
     nextChess: ChessType.red | ChessType.black
 }
 
-class GameCmp extends React.Component<{},IState> {
+class GameCmp extends React.Component<{}, IState> {
 
-    state:IState = {
-        chesses:[],
-        gameStatus:GameStatus.gaming,
+    state: IState = {
+        chesses: [],
+        gameStatus: GameStatus.gaming,
         nextChess: ChessType.black
     }
 
@@ -23,17 +23,17 @@ class GameCmp extends React.Component<{},IState> {
     }
 
     // 初始化数据
-    init(){
+    init() {
         const arr: ChessType[] = [];
-        for(let i = 0; i < 9; i++) {
+        for (let i = 0; i < 9; i++) {
             arr.push(ChessType.none);
         }
         this.setState({
-            chesses:arr,
+            chesses: arr,
             gameStatus: GameStatus.gaming,
             nextChess: ChessType.black
         })
-    }   
+    }
 
     /**
      * @description: 处理棋子点击事件
@@ -42,40 +42,38 @@ class GameCmp extends React.Component<{},IState> {
      * 2. 点击的位置没有棋子
      * @param {number} index 棋子的位置
      * @return {*}
-     */    
+     */
     handleChessClick(index: number): void {
-        console.log( this.state.gameStatus )
-        const chesses:ChessType[] = [...this.state.chesses];
+        const chesses: ChessType[] = [...this.state.chesses];
         chesses[index] = this.state.nextChess;
-        this.setState( prev => ({
+        this.setState(prev => ({
             chesses,
             nextChess: prev.nextChess === ChessType.red ? ChessType.black : ChessType.red,
-            gameStatus: this.getGameStatus(chesses,index)
+            gameStatus: this.getGameStatus(chesses, index)
         }))
     }
 
-    getGameStatus(chesses:ChessType[], index: number): GameStatus{
-        console.log(" game status")
+    getGameStatus(chesses: ChessType[], index: number): GameStatus {
         // 1. 判断是否有一方获得胜利
         const horMin = Math.floor(index / 3) * 3;
         const verMin = index % 3;
-        if( (chesses[horMin] === chesses[horMin + 1] || chesses[horMin] === chesses[horMin + 2] )  // 横向三连
-            || 
-            ( chesses[verMin] === chesses[verMin + 3] || chesses[verMin] === chesses[verMin + 6] )  //纵向三连
-            || 
-            ( chesses[2] === chesses[4] && chesses[2] === chesses[6] && chesses[2] !== ChessType.none ) //对角线三连
+        if ((chesses[horMin] === chesses[horMin + 1] && chesses[horMin] === chesses[horMin + 2]) // 横向三连
             ||
-            (chesses[0] === chesses[4] && chesses[0] === chesses[8] && chesses[0] !== ChessType.none) //对角线三连
-        ){
-            console.log(' who is win ')
-            if(chesses[index] === ChessType.red){
+            (chesses[verMin] === chesses[verMin + 3] && chesses[verMin] === chesses[verMin + 6]) // 纵向三连
+            ||
+            (chesses[0] === chesses[4] && chesses[0] === chesses[8] && chesses[0] !== ChessType.none) // 对角线三连
+            ||
+            (chesses[2] === chesses[4] && chesses[2] === chesses[6] && chesses[2] !== ChessType.none))  // 对角线三连
+        {
+
+            if (chesses[index] === ChessType.red) {
                 return GameStatus.redWin;
-            }else{
+            } else {
                 return GameStatus.blackWin
             }
         }
-            // 2.判断是否平局
-        if(!chesses.includes(ChessType.none)){
+        // 2.判断是否平局
+        if (!chesses.includes(ChessType.none)) {
             return GameStatus.equal;
         }
         // 3.游戏正在进行
@@ -86,10 +84,10 @@ class GameCmp extends React.Component<{},IState> {
         return (
             <div className="game">
                 <h1>三连棋游戏</h1>
-                <BoardCmp 
+                <BoardCmp
                     chesses={this.state.chesses}
                     isGameOver={this.state.gameStatus !== GameStatus.gaming}
-                    onClick={ this.handleChessClick.bind(this) }
+                    onClick={this.handleChessClick.bind(this)}
                 />
                 <button onClick={() => { this.init() }} >重新开始</button>
             </div>
